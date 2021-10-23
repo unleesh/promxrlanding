2896b7f50d952f714e98b85671ae262044bd593f
unleesh <unorhe@gmail.com>
2021-10-24 01:08:35 +0900

    remove producthunt float

--- _src/index.js
+++ _src/index.js
@@ -4,9 +4,6 @@ import './index.scss';
 import { default as Tobi } from '@midzer/tobii'
 const tobi = new Tobi()
 
-// Remove the two following lines to remove the product hunt floating prompt
-import FloatingPrompt from 'producthunt-floating-prompt'
-FloatingPrompt({ name: '싱글들의 만남에 참여하세요', url: 'https://discord.gg/jKcrNExZA4', bottom: '96px', width: '450px' })
 
 // Remove the following lines to remove the darkmode js
 // import Darkmode from 'darkmode-js'
