5b540524397256a7df77737e57a9bd00ddabfa32
unleesh <unorhe@gmail.com>
2021-10-24 00:52:58 +0900

    change to prom

--- _src/index.js
+++ _src/index.js
@@ -6,11 +6,11 @@ const tobi = new Tobi()
 
 // Remove the two following lines to remove the product hunt floating prompt
 import FloatingPrompt from 'producthunt-floating-prompt'
-FloatingPrompt({ name: 'Mobile App Landing Page', url: 'https://www.producthunt.com/posts/mobile-app-landing-page', bottom: '96px', width: '450px' })
+FloatingPrompt({ name: '싱글들의 만남에 참여하세요', url: 'https://discord.gg/jKcrNExZA4', bottom: '96px', width: '450px' })
 
 // Remove the following lines to remove the darkmode js
-import Darkmode from 'darkmode-js'
-function addDarkmodeWidget() {
-  new Darkmode().showWidget()
-}
-window.addEventListener('load', addDarkmodeWidget)
+// import Darkmode from 'darkmode-js'
+// function addDarkmodeWidget() {
+//   new Darkmode().showWidget()
+// }
+// window.addEventListener('load', addDarkmodeWidget)
